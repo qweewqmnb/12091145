ce8c28db77cf7f6fa9d60d6d68d8cc7298c2f324
oo <uioertert@163.com>
2018-12-09 16:05:52 +0800

    1605

--- src/main/java/com/abc2/pppppp.java
+++ src/main/java/com/abc2/pppppp.java
@@ -3,5 +3,6 @@ package com.abc2;
 public class pppppp {
     public static void main(String[] args) {
         System.out.println("ooooooooooooooooo2222222222222222222222");
+        System.out.println("1");
     }
 }
